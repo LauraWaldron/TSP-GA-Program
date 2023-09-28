9ebfefc796a206e28fa3f0ba61904abea2abd351
LauraWaldron <lwaldron2021@fau.edu>
2023-09-28 11:41:02 -0400

    edited files

--- tsp.py
+++ tsp.py
@@ -166,3 +166,20 @@ for i in range(0, 25):
     cityList.append(City(x=int(random.random() * 200), y=int(random.random() * 200)))
 
 geneticAlgorithm(population=cityList, popSize=100, eliteSize=20, mutationRate=0.01, generations=500)
+
+def geneticAlgorithmPlot(population, popSize, eliteSize, mutationRate, generations):
+    pop = initialPopulation(popSize, population)
+    progress = []
+    progress.append(1 / rankRoutes(pop)[0][1])
+    
+    for i in range(0, generations):
+        pop = nextGeneration(pop, eliteSize, mutationRate)
+        progress.append(1 / rankRoutes(pop)[0][1])
+    
+    plt.plot(progress)
+    plt.ylabel('Distance')
+    plt.xlabel('Generation')
+    plt.show()
+    
+geneticAlgorithmPlot(population=cityList, popSize=100, eliteSize=20, mutationRate=0.01, generations=500)
+
